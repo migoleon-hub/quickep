3b4240706dbaa472c82882b8da58d70a787ec8a2
migoleon-hub <migoleon@outlook.com>
2024-11-10 02:17:53 +0200

    Create user.py

---
+++ api/models/user.py
@@ -0,0 +1,14 @@
+from api import db
+from datetime import datetime
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(255), nullable=False)
+    first_name = db.Column(db.String(50))
+    last_name = db.Column(db.String(50))
+    created_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    is_active = db.Column(db.Boolean, nullable=False, default=True)
+    
+    def __repr__(self):
+        return f'<User {self.email}>'
