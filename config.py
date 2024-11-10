1aca08a15318dcd248b127072ad843eff3a422b3
migoleon-hub <migoleon@outlook.com>
2024-11-10 12:07:30 +0200

    Update config.py

--- config.py
+++ config.py
@@ -4,6 +4,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'sqlite:///fastkep.db')
+    DATABASE_URL = f"postgresql://quickep_admin:MiGoLeOn%40%40_7@quickep-db.postgres.database.azure.com:5432/postgres"
+    SQLALCHEMY_DATABASE_URI = DATABASE_URL
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.getenv('SECRET_KEY', 'dev-secret-key-change-me')
