3e6fbafbf53d834b5db808d3e3e6bbe3268efcf4
migoleon-hub <migoleon@outlook.com>
2024-11-12 11:02:25 +0200

    Update manage.py

--- manage.py
+++ manage.py
@@ -1,13 +1,1 @@
-from flask.cli import FlaskGroup
-from api import create_app, db
-from api.models import User
 
-cli = FlaskGroup(create_app=create_app)
-
-@cli.command("create_db")
-def create_db():
-    db.create_all()
-    db.session.commit()
-
-if __name__ == '__main__':
-    cli()
